7f67d3aed2eed0fbda6939800b4994885482e9e8
Jocelyn Pender <jpend049@uottawa.ca>
2025-02-14 09:37:22 -0500

    create jupyter notebook so that I can view result easily

--- get_crossref_data.py
+++ get_crossref_data.py
@@ -1,19 +1,21 @@
 import requests
+import pandas as pd
 
 def get_crossref_data(doi):
     url = f"https://api.crossref.org/works/{doi}"
     response = requests.get(url)
     
     if response.status_code == 200:
-        return response.json()
+        data = response.json()
+        return pd.json_normalize(data)
     else:
         return None
 
 if __name__ == "__main__":
-    doi = "10.1038/nphys1170"  # Example DOI
+    doi = "10.1016/j.jacc.2017.11.012"  # Example DOI
     data = get_crossref_data(doi)
     
-    if data:
+    if data is not None:
         print(data)
     else:
         print("Failed to retrieve data")
